cf6f5beac19771717bf4479fb85c1abec5717fe2
Ivan K <vivenchik@gmail.com>
2023-09-13 19:21:06 +0300

    fix
    Signed-off-by: Ivan K <vivenchik@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'smarthouse'
-copyright = '2023, Ivan Kriuchkov'
-author = 'Ivan Kriuchkov'
-release = '2.0.0b1'
+project = "smarthouse"
+copyright = "2023, Ivan Kriuchkov"
+author = "Ivan Kriuchkov"
+release = "2.0.0b1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
